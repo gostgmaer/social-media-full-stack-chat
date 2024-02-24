7e1f2bc0a3bce9965489fd668f5537efb7eb76f3
Kishor Sarkar <kishor.sarkar@inadev.com>
2024-02-24 20:12:02 +0530

    updated page for data

--- social-media-socket/src/app/profile/page.js
+++ social-media-socket/src/app/profile/page.js
@@ -1,14 +1,15 @@
-import Layout from '@/components/global/layout'
-import { ProfileDetails } from '@/components/pages/profile/elements'
-import React from 'react'
+import Layout from "@/components/global/layout";
+import { ProfileDetails } from "@/components/pages/profile/elements";
+import React from "react";
 
 const Page = () => {
   return (
     <Layout>
-
-      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex items-start justify-start gap-5 mt-5 mb-5'><ProfileDetails /></div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 flex items-start justify-start gap-5 mt-5 mb-5">
+        <ProfileDetails />
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- social-media-socket/src/components/forms/auth/changePassword.js
+++ social-media-socket/src/components/forms/auth/changePassword.js
@@ -1,47 +1,30 @@
 "use client";
+import Input from "@/components/global/fields/input";
 import { post } from "@/utils/lib/network/http";
 import { notifySuccess } from "@/utils/notify/notice";
 import { resetPasswordValidation } from "@/utils/validation/validationSchema";
-// import PasswordField from "@/components/global/fields/PasswordField";
-// import { useAuthContext } from "@/context/AuthContext";
-// import { post } from "@/lib/network/http";
-
-// import { useAxios } from "@/lib/network/interceptors";
-// import { notifySuccess } from "@/lib/notify/notification";
-
-// import { resetPasswordValidation } from "@/utils/validation/validation";
-// import { KeyboardArrowRight } from "@mui/icons-material";
 import { useFormik } from "formik";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const ChangePassword = () => {
-  // const { handleLoginAuth, user, userId } = useAuthContext();
   const router = useRouter();
-
   const [error, setError] = useState(undefined);
   const param = useSearchParams();
 
   const handleSubmit = async (values) => {
     try {
-      const reset = await post(
-        `/user/auth/change-password`,
-        { password: values.password }
-      );
+      const reset = await post(`/user/auth/change-password`, {
+        password: values.password,
+      });
       if (reset.status == "OK") {
-       notifySuccess(reset.message,2000)
+        notifySuccess(reset.message, 2000);
       }
     } catch (error) {
       const myErr = error?.["message"];
       setError(JSON.parse(myErr));
     }
   };
-
-  // useEffect(() => {
-  //   if (userId) {
-  //     router.push("/dashboard");
-  //   }
-  // }, [userId]);
 
   const formik = useFormik({
     initialValues: {
@@ -50,63 +33,78 @@ const ChangePassword = () => {
       confirmPassword: "",
     },
     validationSchema: resetPasswordValidation,
-    validateOnBlur: true,
-    validateOnChange: true,
-    onSubmit: (values) => {
+    onSubmit: (values, actions) => {
+      actions.setSubmitting(true);
       handleSubmit(values);
+      actions.setSubmitting(false);
     },
   });
 
   return (
-    <form onSubmit={formik.handleSubmit} className="w-[50%]">
-      <div className="space-y-5 text-black">
-        <div className="rizzui-password-root flex flex-col [&amp;>label>span]:font-medium">
-          <label className="block">
-            <span className="rizzui-password-label block text-sm mb-1.5">
-              Current Password
-            </span>
-            <PasswordField
-              placeholder="current password"
-              name="current_password"
-              value={formik.values.current_password}
-              handleChange={formik.handleChange}
-            />
-          </label>
+    <form
+      onSubmit={formik.handleSubmit}
+      className=" w-full m-auto rounded-md shadow-lg p-5 bg-gray-50 "
+    >
+      <div className="grid  gap-5 sm:grid-cols-2 col-span-full mb-5 w-full">
+        <div className="col-span-full">
+          <Input
+            label={"Current Password"}
+            type={"password"}
+            additionalAttrs={{
+              ...formik.getFieldProps("current_password"),
+              placeholder: "Password",
+            }}
+            classes={undefined}
+            icon={undefined}
+            id={"current_password"}
+          />
         </div>
-        <div className="rizzui-password-root flex flex-col [&amp;>label>span]:font-medium">
-          <label className="block">
-            <span className="rizzui-password-label block text-sm mb-1.5">
-              Password
-            </span>
-            <PasswordField
-              placeholder="Password"
-              name="password"
-              value={formik.values.password}
-              handleChange={formik.handleChange}
-            />
-          </label>
+        <div className="col-span-full">
+          <Input
+            label={"Password"}
+            type={"password"}
+            additionalAttrs={{
+              ...formik.getFieldProps("password"),
+              placeholder: "Password",
+            }}
+            classes={undefined}
+            icon={undefined}
+            id={"password"}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="text-red-600 capitalize">
+              {formik.errors.password}
+            </div>
+          ) : null}
         </div>
-        <div className="rizzui-password-root flex flex-col [&amp;>label>span]:font-medium">
-          <label className="block" htmlFor="confirm-password">
-            <span className="rizzui-password-label block text-sm mb-1.5">
-              Confirm Password
-            </span>
-            <PasswordField
-              placeholder="Confirm Password"
-              name="confirmPassword"
-              value={formik.values.confirmPassword}
-              handleChange={formik.handleChange}
-            />
-          </label>
+        <div className="col-span-full">
+          <Input
+            label={"Confirm Password"}
+            type={"password"}
+            additionalAttrs={{
+              ...formik.getFieldProps("confirmPassword"),
+              placeholder: "Password",
+            }}
+            classes={undefined}
+            icon={undefined}
+            id={"confirmPassword"}
+          />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+            <div className="text-red-600 capitalize">
+              {formik.errors.confirmPassword}
+            </div>
+          ) : null}
         </div>
 
-        <button
-          className="rizzui-button  disabled:bg-gray-400 inline-flex font-medium items-center justify-center active:enabled:translate-y-px focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-50 transition-colors duration-200 px-8 py-2.5 text-base  rounded-md border border-transparent focus-visible:ring-offset-2 bg-gray-900 hover:enabled::bg-gray-800 active:enabled:bg-gray-1000 focus-visible:ring-gray-900/30 text-gray-0 w-full text-white"
-          type="submit"
-          disabled={!formik.isValid}
-        >
-          <span>Change Password</span> <KeyboardArrowRight />
-        </button>
+        <div className="col-span-full mt-5">
+          <button
+            className="rizzui-button  m-auto  disabled:bg-gray-400 inline-flex font-medium items-center justify-center active:enabled:translate-y-px focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-50 transition-colors duration-200 px-8 py-2.5 text-base  rounded-md border border-transparent focus-visible:ring-offset-2 bg-gray-900 hover:enabled::bg-gray-800 active:enabled:bg-gray-1000 focus-visible:ring-gray-900/30 text-gray-0 w-full text-white"
+            type="submit"
+            disabled={!formik.isValid}
+          >
+            {formik.isSubmitting ? "Submitting..." : "Change"}
+          </button>
+        </div>
       </div>
       {error && (
         <div
